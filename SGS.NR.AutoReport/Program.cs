c9fb397db3658279eaf0553b66ad7abbf74f8347
APAC\Brian_Li <brian.li@sgs.com>
2024-04-08 14:23:16 +0800

    Add project files.

---
+++ SGS.NR.AutoReport/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SGS.NR.AutoReport
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
