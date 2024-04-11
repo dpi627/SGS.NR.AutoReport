2418b4dab361d051ae861df5fd613fe8d52136a8
Brian <brian.li@sgs.com>
2024-04-11 16:10:17 +0800

    主程式導入 DI 導入 Extension 移植範本與處理邏輯

---
+++ SGS.NR.AutoReport/Extension/ServiceExtension.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SGS.NR.Repository.Implement;
+using SGS.NR.Repository.Interface;
+using SGS.NR.Service.Implement;
+using SGS.NR.Service.Interface;
+
+namespace SGS.NR.AutoReport.Extension
+{
+    /// <summary>
+    /// 註冊服務擴充方法
+    /// </summary>
+    public static class ServiceExtension
+    {
+        /// <summary>
+        /// 註冊 Service
+        /// </summary>
+        /// <param name="services">服務集合</param>
+        /// <returns>服務集合</returns>
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddSingleton<IDraftService, DraftService>();
+            return services;
+        }
+
+        /// <summary>
+        /// 註冊 Repository
+        /// </summary>
+        /// <param name="services">服務集合</param>
+        /// <returns>服務集合</returns>
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddSingleton<ITestRecordRepository, TestRecordRepository>();
+            return services;
+        }
+
+        /// <summary>
+        /// 註冊其他服務
+        /// </summary>
+        /// <param name="services">服務集合</param>
+        /// <returns>服務集合</returns>
+        public static IServiceCollection AddMiscs(this IServiceCollection services)
+        {
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            return services;
+        }
+
+        /// <summary>
+        /// 取得或建立服務
+        /// </summary>
+        /// <typeparam name="T">服務類型</typeparam>
+        /// <param name="serviceProvider">服務提供者</param>
+        /// <returns>服務實例</returns>
+        public static T GetOrCreateService<T>(this IServiceProvider serviceProvider)
+        {
+            return serviceProvider.GetService<T>() ?? ActivatorUtilities.CreateInstance<T>(serviceProvider);
+        }
+    }
+}
--- SGS.NR.AutoReport/Program.cs
+++ SGS.NR.AutoReport/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace SGS.NR.AutoReport
+﻿using Microsoft.Extensions.DependencyInjection;
+using SGS.NR.AutoReport.Extension;
+using SGS.NR.Service.DTO.Info;
+using SGS.NR.Service.Interface;
+
+namespace SGS.NR.AutoReport
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 建立 DI 容器，註冊服務
+            IServiceCollection services = new ServiceCollection()
+                .AddServices()
+                .AddRepositories()
+                .AddMiscs();
+
+            // 建立 ServiceProvider
+            IServiceProvider serviceProvider = services.BuildServiceProvider();
+
+            // 透過 ServiceProvider 取得服務 IDraftService 之實例
+            IDraftService? service = serviceProvider.GetOrCreateService<IDraftService>();
+
+            service.Save(new DraftInfo() {
+                ImportPath = @"C:\Users\Brian_Li\OneDrive - SGS\Test\TestRecord_20240403.xlsx",
+                ExportPath = @"C:\dev\_tmp\output\certificate.docx",
+                TemplatePath = @"Template\certificate.docx"
+            });
         }
+
+
     }
 }
