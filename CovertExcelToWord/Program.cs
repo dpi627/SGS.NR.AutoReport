6843bace4970009c9fee6cc09263e6501fc02d88
Brian <brian.li@sgs.com>
2024-09-18 10:32:30 +0800

    逐行讀取資料基本版

---
+++ CovertExcelToWord/Program.cs
@@ -0,0 +1,26 @@
+﻿using MiniExcelLibs;
+using MiniSoftware;
+
+namespace CovertExcelToWord;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = @"C:\dev\_tmp\裝櫃電子表單0401.xlsm";
+        string sheetName = "草稿"; // 替換成您想讀取的工作表名稱
+
+        var rows = MiniExcel.Query(filePath, sheetName: sheetName).ToList();
+
+        foreach (var row in rows)
+        {
+            var rowDict = (IDictionary<string, object>)row;
+            for (char col = 'A'; col <= 'X'; col++)
+            {
+                string cellValue = rowDict[col.ToString()]?.ToString();
+                Console.Write($"{cellValue}\t");
+            }
+            Console.WriteLine(); // 換行,開始下一行
+        }
+    }
+}
