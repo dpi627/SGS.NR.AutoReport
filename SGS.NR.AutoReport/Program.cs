3d62e3a79be034ae17da19aabed8ede479b8b49b
Brian <brian.li@sgs.com>
2024-10-05 07:52:04 +0800

    Refactor to directly use IContainerLoadingService
    Removed unnecessary ServiceProvider creation and directly retrieved IContainerLoadingService using GetOrCreateService. Implemented draft generation with hardcoded paths and output the result to the console.

--- SGS.NR.AutoReport/Program.cs
+++ SGS.NR.AutoReport/Program.cs
@@ -14,18 +14,18 @@ namespace SGS.NR.AutoReport
                 .AddServices()
                 .AddRepositories()
                 .AddMiscs();
-
             // 建立 ServiceProvider
             IServiceProvider serviceProvider = services.BuildServiceProvider();
-
+            // 取得特定服務
             var service = serviceProvider.GetOrCreateService<IContainerLoadingService>();
+            // 產生草稿
             var result = service.GetDraft(new ContainerLoadingInfo()
             {
                 SourcePath = @"C:\dev\_tmp\裝櫃電子表單0401.xlsm",
                 TemplatePath = @"Templates\Draft.Container.Loading.docx",
                 TargetPath = @$"C:\dev\_tmp\{DateTime.Now:yyyyMMddHHmmss}.docx"
             });
-
+            // 輸出結果
             Console.WriteLine($"{result}");
         }
     }
