3f5b92b86edc5a6723083cd3133ecb0cdc668fa1
Brian <brian.li@sgs.com>
2024-10-05 14:25:41 +0800

    導入 Microsoft.Extensions.Hosting 設定基本 Logger 機制 完成組態檔 Configuration 導入 完成服務轉換並可啟動應用程式(host) 實作取出服務 (logger config service)

--- SGS.NR.AutoReport/Program.cs
+++ SGS.NR.AutoReport/Program.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SGS.NR.AutoReport.DTOs;
 using SGS.NR.AutoReport.Extension;
 using SGS.NR.Service.DTO.Info;
 using SGS.NR.Service.Interface;
@@ -9,24 +13,42 @@ namespace SGS.NR.AutoReport
     {
         static void Main(string[] args)
         {
-            // 建立 DI 容器，註冊服務
-            IServiceCollection services = new ServiceCollection()
-                .AddServices()
+            var builder = Host.CreateApplicationBuilder();
+
+            builder.Logging.AddConsole();
+
+            builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            builder.Services.AddOptions<ContainerLoading>().BindConfiguration("appsettings.json");
+
+            builder.Services.AddServices()
                 .AddRepositories()
                 .AddMiscs();
-            // 建立 ServiceProvider
-            IServiceProvider serviceProvider = services.BuildServiceProvider();
-            // 取得特定服務
-            var service = serviceProvider.GetOrCreateService<IContainerLoadingService>();
-            // 產生草稿
-            var result = service.GetDraft(new ContainerLoadingInfo()
+
+            var host = builder.Build();
+
+            // 取得 Logger 服務
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+            // 取得 Configuration 服務
+            var config = host.Services.GetRequiredService<IConfiguration>();
+            // 讀取特定 section 並轉換為強型別
+            var containerLoading = config.GetSection("ContainerLoading").Get<ContainerLoading>();
+            // 取得 ContainerLoading 服務
+            var service = host.Services.GetRequiredService<IContainerLoadingService>();
+            // 建立 ContainerLoadingInfo 物件
+            var info = new ContainerLoadingInfo()
             {
-                SourcePath = @"C:\dev\_tmp\裝櫃電子表單0401.xlsm",
-                TemplatePath = @"Templates\Draft.Container.Loading.docx",
-                TargetPath = @$"C:\dev\_tmp\{DateTime.Now:yyyyMMddHHmmss}.docx"
-            });
-            // 輸出結果
-            Console.WriteLine($"{result}");
+                SourcePath = containerLoading.SourcePath,
+                TemplatePath = containerLoading.TemplatePath,
+                TargetPath = string.Format(containerLoading.TargetPath, DateTime.Now.ToString("yyyyMMddHHmmss"))
+            };
+            // 呼叫 ContainerLoadingService 服務
+            var result = service.GetDraft(info);
+
+            logger.LogInformation("ContainerLoadingService.GetDraft() called.");
+
+            host.Run();
         }
     }
 }
