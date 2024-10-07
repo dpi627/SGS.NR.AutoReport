462d92015795988a6c80bbf6bc77c2004180625c
Brian <brian.li@sgs.com>
2024-10-07 16:46:44 +0800

    完成 WPF 基底專案重構 導入 Hosting 設計框架 導入依賴注入，移除預設程式

--- SGS.NR.AutoReport.Wpf/App.xaml.cs
+++ SGS.NR.AutoReport.Wpf/App.xaml.cs
@@ -1,14 +1,37 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
 
 namespace SGS.NR.AutoReport.Wpf
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private readonly IHost _host;
+
+        public App()
+        {
+            var builder = Host.CreateApplicationBuilder();
+
+            builder.Services.AddSingleton<MainWindow>();
+
+            _host = builder.Build();
+        }
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await _host.StartAsync();
+            base.OnStartup(e);
+            var mainWindow = _host.Services.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await _host.StopAsync();
+            _host.Dispose();
+            base.OnExit(e);
+        }
     }
 
 }
