3656b0b4eebc2dabbe10b055ccda31a9aaf27e9e
Brian <brian.li@sgs.com>
2024-10-29 11:58:18 +0800

    完成貨品 24304166 資料結構

---
+++ SGS.NR.Repository/DTO/DataModel/VesselUnload24304166/SingleDataModel.cs
@@ -0,0 +1,39 @@
+﻿#nullable disable
+
+namespace SGS.NR.Repository.DTO.DataModel.VesselUnload24304166;
+
+public record SingleDataModel
+{
+    public string HeaderService { get; set; }
+    public string HeaderCompany { get; set; }
+    public string HeaderNo { get; set; }
+    public string ReportTitle { get; set; }
+    public string DateLocation { get; set; }
+    public string Goods { get; set; }
+    public string Goods2 { get; set; }
+    public string Goods3 { get; set; }
+    //public string Shipment { get; set; }
+    public string Shipper { get; set; }
+    public string Trader { get; set; }
+    public string Buyer { get; set; }
+    public string Packing { get; set; }
+    public string Marks { get; set; }
+    public string Stowage { get; set; }
+    public string Discharge { get; set; }
+    public string QuantityDelivered { get; set; }
+    public string Inspection { get; set; }
+    public string Inspection2 { get; set; }
+    public string Remarks { get; set; }
+    public string Remarks2 { get; set; }
+    public string FooterLeft { get; set; }
+    public string FooterRight { get; set; }
+
+    public string GoodsTotalNetWeight { get; set; }
+    public string GoodsTotalNetWeightUnit { get; set; }
+    public string GoodsTotalGrossWeight { get; set; }
+    public string GoodsTotalGrossWeightUnit { get; set; }
+    public string GoodsTotalPackage { get; set; }
+    public string GoodsTotalPackageUnit { get; set; }
+
+    public string TotalGoodsQuantity { get; set; }
+}
