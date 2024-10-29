680ee3cd5aab724f76c0f6103aa50ac2ea8e33fd
Brian <brian.li@sgs.com>
2024-10-29 10:36:57 +0800

    完成貨品 24301715 定義

---
+++ SGS.NR.Repository/DTO/DataModel/VesselUnload24301715/MainDataModel.cs
@@ -0,0 +1,13 @@
+﻿namespace SGS.NR.Repository.DTO.DataModel.VesselUnload24301715;
+
+public record MainDataModel
+{
+    public MainDataModel()
+    {
+        SingleData = new SingleDataModel();
+        CollectionData = new CollectionDataModel();
+    }
+
+    public SingleDataModel SingleData { get; set; }
+    public CollectionDataModel CollectionData { get; set; }
+}
---
+++ SGS.NR.Repository/DTO/DataModel/VesselUnload24301715/SingleDataModel.cs
@@ -0,0 +1,37 @@
+﻿#nullable disable
+
+namespace SGS.NR.Repository.DTO.DataModel.VesselUnload24301715;
+
+public record SingleDataModel
+{
+    public string HeaderService { get; set; }
+    public string HeaderCompany { get; set; }
+    public string HeaderNo { get; set; }
+    public string ReportTitle { get; set; }
+    public string DateLocation { get; set; }
+    public string Goods { get; set; }
+    public string Goods2 { get; set; }
+    public string Goods3 { get; set; }
+    public string Shipper { get; set; }
+    public string Buyer { get; set; }
+    public string Packing { get; set; }
+    public string Marks { get; set; }
+    public string Stowage { get; set; }
+    public string Discharge { get; set; }
+    public string QuantityDelivered { get; set; }
+    public string Inspection { get; set; }
+    public string Remarks { get; set; }
+    public string Remarks2 { get; set; }
+    public string Remarks3 { get; set; }
+    public string FooterLeft { get; set; }
+    public string FooterRight { get; set; }
+
+    public string GoodsTotalNetWeight { get; set; }
+    public string GoodsTotalNetWeightUnit { get; set; }
+    public string GoodsTotalGrossWeight { get; set; }
+    public string GoodsTotalGrossWeightUnit { get; set; }
+    public string GoodsTotalQuantity { get; set; }
+    public string GoodsTotalQuantityUnit { get; set; }
+
+    public string QuantityTotalCargo { get; set; }
+}
