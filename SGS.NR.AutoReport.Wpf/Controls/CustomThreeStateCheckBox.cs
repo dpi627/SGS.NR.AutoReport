e84acb640adad2af84df0b03f9db939848811ffd
Brian <brian.li@sgs.com>
2024-11-20 16:08:09 +0800

    新增三態自訂 checkbox Page 新增 datagrid header template，使用 custom checkbox ViewModel 完成全選勾選相關功能

---
+++ SGS.NR.AutoReport.Wpf/Controls/CustomThreeStateCheckBox.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows.Controls;
+
+namespace SGS.NR.AutoReport.Wpf.Controls;
+
+public class CustomThreeStateCheckBox : CheckBox
+{
+    protected override void OnToggle()
+    {
+        if (IsChecked == null)
+        {
+            IsChecked = true;
+        }
+        else if (IsChecked == true)
+        {
+            IsChecked = false;
+        }
+        else
+        {
+            IsChecked = true;
+        }
+    }
+}
+
